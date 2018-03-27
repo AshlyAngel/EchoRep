f1e8038473028474d8ff755623b516defe10b4e4
ashly <elsyantonybabu@gmail.com>
2018-03-27 15:26:47 +0530

    Server modified

--- EchoServer.java
+++ EchoServer.java
@@ -18,7 +18,15 @@ public class EchoServer
 	{
 		try
 		{
-		
+			while (true)
+			{
+				Socket client = server.accept();
+				BufferedReader r = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				PrintWriter w = new PrintWriter(client.getOutputStream(), true);
+				w.println("Welcome to the Java EchoServer.  Type 'bye' to close.");
+				String line;
+				do
+				{
 		}
 		catch(Exception err)
 		{
