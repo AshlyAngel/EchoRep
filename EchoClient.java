54cd6db529aef24d7857d271473862c3e5cc718f
AnjaliTB <anjalitbaiju@gmail.com>
2018-04-02 23:45:04 +0530

    Client modified

--- EchoClient.java
+++ EchoClient.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.*;
-public class EchoClient{
+public class EchoClient
+{
     public static void main(String args[])throws Exception
     {
     	Socket sock = new Socket("127.0.0.1", 3000);
@@ -18,13 +19,12 @@ public class EchoClient{
         	pwrite.println(sendMessage);
         	pwrite.flush();  
         	receiveMessage = receiveRead.readLine(); 
+        	System.out.println("Server: " + receiveMessage); 
         	if((sendMessage.equalsIgnoreCase("ok"))||(receiveMessage.equalsIgnoreCase("ok")))
         	{
        			System.out.println("Client exiting..."); 
-			System.exit(0);
-			
+			System.exit(0);	
         	}
      	}while(true) ; 
     }
-
 }
