fb2f9d28f2e78942e0e71edb71a6c38ddee7ac13
AnjaliTB <anjalitbaiju@gmail.com>
2018-04-02 23:14:53 +0530

    Client modified

--- EchoClient.java
+++ EchoClient.java
@@ -3,6 +3,9 @@ import java.net.*;
 public class EchoClient{
     public static void main(String args[])throws Exception
     {
+    	Socket sock = new Socket("127.0.0.1", 3000);
+     	BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+     	OutputStream ostream = sock.getOutputStream(); 
     
     }
 
